130c3e4d2e348a0ae7381f3db51583dcc188fc2e
kacha930 <robiwere20@gmail.com>
2024-06-26 21:04:21 +0300

    success

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-
 // Write your code here
+//question 1
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply);
+ 
+//question 2 REVISIT
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random); 
+
+
+//question 3
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+
+//question 4 
+Math.max(0, 5, 10, 15, 20);
+const max = 20;
+
